e1488697df1e4d8465061016196298ecb48dcd62
SirPudwhip <spamfolder3399@gmail.com>
2023-03-16 10:51:50 -0400

    Maybe finished? Who knows

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+
+    def __init__(self, discount=0):
+        self.discount = discount 
+        self.total = 0
+        self.items = []
+        self.item_prices = []
+
+    def add_item(self, title, price, quantity = 1):
+        i = 0 
+        while(i < quantity):
+            self.total = self.total + price 
+            self.items.append(title)
+            self.item_prices.append(price)
+
+    def apply_discount(self):
+        if (self.discount == 0): 
+            print("There is no discount to apply.")
+        
+        else: 
+            disc = self.discount/100
+            self.total = int(self.total-(self.total*disc))
+            print(f"After the discount, the total comes to ${self.total}.")
+
+    def void_last_transaction(self):
+        self.items.pop()
+        void_cost = self.item_prices.pop()
+        self.total = self.total-void_cost
+
+
+
+# testRegister = CashRegister()
+# testRegister.add_item("beans", 5)
+# testRegister.add_item("eggs", 5)
+# testRegister.add_item("cheese", 5)
+# testRegister.add_item("milk", 5)
+
+# print(testRegister.items)
+# print(testRegister.total)
+
